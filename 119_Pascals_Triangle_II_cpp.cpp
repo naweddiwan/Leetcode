61b36f466fb9cda1fa10f77b7cff281ea91b44ac
Nawed Diwan <40795334+naweddiwan@users.noreply.github.com>
2020-03-21 19:22:42 +0530

    Create 119_Pascals_Triangle_II_cpp.cpp

---
+++ 119_Pascals_Triangle_II_cpp.cpp
@@ -0,0 +1,30 @@
+vector<int> getRow(int rowIndex)
+{
+
+    int **arr = new int *[rowIndex + 1];
+    for (int i = 0; i <= rowIndex; i++)
+    {
+        arr[i] = new int[i + 1];
+    }
+    vector<int> res;
+    for (int i = 0; i <= rowIndex; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            if (j == 0 || j == i)
+                arr[i][j] = 1;
+            else
+                arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+
+            if (i == rowIndex)
+            {
+                res.push_back(arr[i][j]);
+            }
+        }
+    }
+    for (int i = 0; i <= rowIndex; i++)
+        delete[] arr[i];
+
+    delete[] arr;
+    return res;
+}
