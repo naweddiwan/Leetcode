5537968f27619d545a84d566455d10fa2acbe746
Nawed Diwan <40795334+naweddiwan@users.noreply.github.com>
2020-03-24 02:17:12 +0530

    Create 1002. Find Common Characters.cpp

---
+++ 1002. Find Common Characters.cpp
@@ -0,0 +1,32 @@
+ vector<string> commonChars(vector<string>& A) {
+        vector<map<char, int>> m(A.size());
+        int i=0;
+        map<char, int> s;
+        vector<string> res;
+        for(auto strings : A)
+        {
+            for(auto k : strings)
+            {
+               s[k]++;
+                (m[i])[k]++;
+            }
+            i++;
+        }
+        int min1 =INT_MAX;
+        for(auto it:s)
+        {
+            for(auto i : m)
+            {
+                min1 = min(min1, i[it.first]);
+            }
+            
+            while(min1--)
+            {
+                string s1;
+                s1+=it.first;
+                res.push_back(s1);
+            }
+            min1 = INT_MAX;
+        }
+        return res;
+    }
